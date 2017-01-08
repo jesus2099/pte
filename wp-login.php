e2c241791456120d1fa36a2ed5df9f56439b19f0
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2017-01-08 23:14:21 +0100

    wayback_machine_downloader paris-taiko.com with and without option -a then merged @hartator’s wayback-machine-downloader

---
+++ wp-login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="fr-FR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="fr-FR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Paris Taiko Ensemble &rsaquo; Se connecter</title>
+	<link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=3.8.12' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://paris-taiko.com/wp-includes/css/dashicons.min.css?ver=3.8.12' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-admin-css'  href='http://paris-taiko.com/wp-admin/css/wp-admin.min.css?ver=3.8.12' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://paris-taiko.com/wp-includes/css/buttons.min.css?ver=3.8.12' type='text/css' media='all' />
+<link rel='stylesheet' id='colors-fresh-css'  href='http://paris-taiko.com/wp-admin/css/colors.min.css?ver=3.8.12' type='text/css' media='all' />
+<!--[if lte IE 7]>
+<link rel='stylesheet' id='ie-css'  href='http://paris-taiko.com/wp-admin/css/ie.min.css?ver=3.8.12' type='text/css' media='all' />
+<![endif]-->
+<meta name='robots' content='noindex,follow' />
+	</head>
+	<body class="login login-action-login wp-core-ui">
+	<div id="login">
+		<h1><a href="http://www.wordpress-fr.net/" title="Propulsé par WordPress">Paris Taiko Ensemble</a></h1>
+	
+<form name="loginform" id="loginform" action="http://paris-taiko.com/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Identifiant<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Mot de passe<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Se souvenir de moi</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Se connecter" />
+		<input type="hidden" name="redirect_to" value="http://paris-taiko.com/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://paris-taiko.com/wp-login.php?action=lostpassword" title="Récupération de mot de passe">Mot de passe oublié&nbsp;?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://paris-taiko.com/" title="Êtes-vous perdu(e)&nbsp;?">&larr; Retour sur Paris Taiko Ensemble</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
